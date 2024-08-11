22fe83ae2fd80588bd0b33d92889e52277a322af
umenyioraj <129884687+umenyioraj@users.noreply.github.com>
2024-08-11 10:23:32 -0400

    changed env

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 app.use(bodyParser.json());
 
@@ -19,8 +21,8 @@ let events = [];
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'umenyiorajosh@gmail.com',
-  password: 'Joshua10**',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: 'events_db'
 });
 
